10c930a1dcf0531c6c72831cddbf61c55d5f5404
oussama <khemilioussama775@gmail.com>
2021-02-17 23:59:00 +0100

    commande sh

---
+++ commande.sh
@@ -0,0 +1,31 @@
+# afficher répertoire
+pwd
+
+#afficher contenu de dossier
+ls 
+
+#acceder dossier
+cd nom_dossier
+
+#retour en arriere
+cd ..
+
+#retour racines
+cd / 
+
+#creer un dossier
+sudo mkdir nom_dossier
+
+#supprimer dossier
+sudo rm -r nom_dossier
+
+#creer fichier
+touch nom_fichier.ext
+
+#afficher contenu fichier
+more nom_fichier
+
+#modifier fichier
+sudo nano nom_fichier
+
+
